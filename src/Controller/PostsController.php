32df6be470287295fd6d3ec216ce86c6c3a0af74
Lucas Sinica <64593019+LukaszSinica@users.noreply.github.com>
2025-03-27 21:27:26 +0100

    Security

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -10,8 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/posts')]
+#[IsGranted('ROLE_ADMIN', statusCode: 423)]
 final class PostsController extends AbstractController
 {
     #[Route(name: 'app_posts_index', methods: ['GET'])]
